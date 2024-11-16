0413a72f7d7c4a9ba8e3865e80ec2c4a6f810fdb
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2024-11-17 00:17:06 +0600

    admin chart added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        edunestSky: "#C3EBFA",
+        edunestSkyLight: "#EDF9FD",
+        edunestPurple: "#CFCEFF",
+        edunestPurpleLight: "#F1F0FF",
+        edunestYellow: "#FAE27C",
+        edunestYellowLight: "#FEFCE8",
+      }
     },
   },
   plugins: [],
