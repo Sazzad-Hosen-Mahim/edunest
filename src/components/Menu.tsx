3b7c6f43dd06980d562802abf8ad2531643f1593
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2024-11-16 12:39:46 +0600

    Dashboard navbar and menu bar completed statically

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
      {
        title: "MENU",
@@ -112,3 +115,26 @@ const menuItems = [
        ],
      },
    ];
+
+
+   const Menu = () => {
+     return (
+       <div className='mt-4 text-sm'>
+        {menuItems.map(i=> (
+          <div className="flex flex-col gap-2" key={i.title}>
+            <span className="hidden lg:block text-gray-400 my-4">{i.title}</span>
+            {
+              i.items.map(item=> (
+                <Link key={item.label} href={item.href} className="flex gap-2 items-center justify-center lg:justify-start text-gray-500 py-2">
+                  <Image src={item.icon} alt="" width={20} height={20}/>
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              ))
+            }
+          </div>
+        ))}
+       </div>
+     )
+   }
+   
+   export default Menu
