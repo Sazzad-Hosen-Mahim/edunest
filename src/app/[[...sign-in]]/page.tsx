4f57226b8a2ab0a86041c6cd1efc0ccc0c573673
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2025-01-06 17:09:47 +0600

    default sign in for admin added

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -5,11 +5,14 @@ import * as SignIn from '@clerk/elements/sign-in'
 import { useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const LoginPage = () => {
     const { isLoaded, isSignedIn, user } = useUser()
+
+    const [username, setUsername] = useState('admin')
+    const [password, setPassword] = useState('admin')
 
     const router = useRouter()
 
@@ -34,12 +37,14 @@ const LoginPage = () => {
                     <Clerk.GlobalError className='text-xs text-red-400' />
                     <Clerk.Field name="identifier" className='flex flex-col gap-2'>
                         <Clerk.Label className='text-xs text-gray-500'>Username</Clerk.Label>
-                        <Clerk.Input type='text' required className='p-2 rounded-md ring-1 ring-gray-300' />
+                        <Clerk.Input type='text' value={username}
+                            onChange={(e) => setUsername(e.target.value)} required className='p-2 rounded-md ring-1 ring-gray-300' />
                         <Clerk.FieldError className='text-xs text-red-500' />
                     </Clerk.Field>
                     <Clerk.Field name="password" className='flex flex-col gap-2'>
                         <Clerk.Label className='text-xs text-gray-500'>Password</Clerk.Label>
-                        <Clerk.Input type='password' required className='p-2 rounded-md ring-1 ring-gray-300' />
+                        <Clerk.Input type='password' value={password}
+                            onChange={(e) => setPassword(e.target.value)} required className='p-2 rounded-md ring-1 ring-gray-300' />
                         <Clerk.FieldError className='text-xs text-red-500' />
                     </Clerk.Field>
                     <SignIn.Action submit className='bg-blue-500 w-full p-2 mt-5 text-center text-white rounded-md shadow-lg hover:bg-blue-400 hover:shadow-sm hover:text-black'>Sign in</SignIn.Action>
